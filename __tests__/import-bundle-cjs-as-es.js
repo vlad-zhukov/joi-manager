bb0948400fdfe8d5997c46b8b8ae038367f565cd
Vlad Zhukov <work4vlad@gmail.com>
2017-05-14 20:59:19 +0300

    Simplify tests

--- __tests__/import-bundle-cjs-as-es.js
+++ __tests__/import-bundle-cjs-as-es.js
@@ -8,33 +8,27 @@ const OBJECT__ = {};
 const NULL__ = null;
 const SYMBOL__ = Symbol(STRING__);
 
-describe('It should construct', () => {
-    test('it should construct with 0 or 1 arguments', (done) => {
+describe('import-bundle-cjs-as-es', () => {
+    it('should construct with 0 or 1 arguments', () => {
         expect(new JoiManager()).toBeInstanceOf(JoiManager);
         expect(new JoiManager(OBJECT__)).toBeInstanceOf(JoiManager);
         expect(new JoiManager({joi: STRING__})).toBeInstanceOf(JoiManager);
         expect(new JoiManager(new Error())).toBeInstanceOf(JoiManager);
 
         expect(new JoiManager(OBJECT__).defaultOptions).toBe(OBJECT__);
-
-        done();
     });
 
-    test('it should throw if "defaultOptions" is not an object', (done) => {
+    it('should throw if "defaultOptions" is not an object', () => {
         expect(() => new JoiManager(NULL__)).toThrow(TypeError);
         expect(() => new JoiManager(NUMBER__)).toThrow(TypeError);
         expect(() => new JoiManager(STRING__)).toThrow(TypeError);
         expect(() => new JoiManager(SYMBOL__)).toThrow(TypeError);
-
-        done();
     });
 
-    test('"defaultOptions" should default to an empty object', (done) => {
+    test('"defaultOptions" should default to an empty object', () => {
         const joiManager = new JoiManager();
 
         expect(joiManager.defaultOptions).toEqual({});
         expect(Object.keys(joiManager.defaultOptions)).toHaveLength(0);
-
-        done();
     });
 });
--- __tests__/src.js
+++ __tests__/src.js
@@ -9,43 +9,37 @@ const OBJECT__ = {};
 const NULL__ = null;
 const SYMBOL__ = Symbol(STRING__);
 
-describe('It should construct', () => {
-    test('it should construct with 0 or 1 arguments', (done) => {
+describe('JoiManager#constructor()', () => {
+    it('should construct with 0 or 1 arguments', () => {
         expect(new JoiManager()).toBeInstanceOf(JoiManager);
         expect(new JoiManager(OBJECT__)).toBeInstanceOf(JoiManager);
         expect(new JoiManager({joi: STRING__})).toBeInstanceOf(JoiManager);
         expect(new JoiManager(new Error())).toBeInstanceOf(JoiManager);
 
         expect(new JoiManager(OBJECT__).defaultOptions).toBe(OBJECT__);
-
-        done();
     });
 
-    test('it should throw if "defaultOptions" is not an object', (done) => {
+    it('should throw if "defaultOptions" is not an object', () => {
         expect(() => new JoiManager(NULL__)).toThrow(TypeError);
         expect(() => new JoiManager(NUMBER__)).toThrow(TypeError);
         expect(() => new JoiManager(STRING__)).toThrow(TypeError);
         expect(() => new JoiManager(SYMBOL__)).toThrow(TypeError);
-
-        done();
     });
 
-    test('"defaultOptions" should default to an empty object', (done) => {
+    test('"defaultOptions" should default to an empty object', () => {
         const joiManager = new JoiManager();
 
         expect(joiManager.defaultOptions).toEqual({});
         expect(Object.keys(joiManager.defaultOptions)).toHaveLength(0);
-
-        done();
     });
 });
 
-describe('It should have "add" method', () => {
+describe('JoiManager#add()', () => {
     const schemaName = 'random-schema-name/number';
     const schemaName2 = 'random-schema-name/number2';
     const schema = Joi.number().min(0).max(1);
 
-    test('it should add new schemas to the list', (done) => {
+    it('should add new schemas to the list', () => {
         const joiManager = new JoiManager();
 
         expect(() => joiManager.add(schemaName, schema)).not.toThrow();
@@ -53,11 +47,9 @@ describe('It should have "add" method', () => {
 
         expect(joiManager.schemaList).toHaveProperty(schemaName, schema);
         expect(joiManager.schemaList).toHaveProperty(schemaName2, schema);
-
-        done();
     });
 
-    test('it should throw if "schemaName" is not a string', (done) => {
+    it('should throw if "schemaName" is not a string', () => {
         const joiManager = new JoiManager();
 
         expect(() => joiManager.add(undefined, schema)).toThrow(TypeError);
@@ -65,46 +57,38 @@ describe('It should have "add" method', () => {
         expect(() => joiManager.add(OBJECT__, schema)).toThrow(TypeError);
         expect(() => joiManager.add(NULL__, schema)).toThrow(TypeError);
         expect(() => joiManager.add(SYMBOL__, schema)).toThrow(TypeError);
-
-        done();
     });
 
-    test('it should throw if "schema" is not defined', (done) => {
+    it('should throw if "schema" is not defined', () => {
         const joiManager = new JoiManager();
 
         expect(() => joiManager.add(schemaName)).toThrow(TypeError);
         expect(() => joiManager.add(schemaName, NULL__)).toThrow(TypeError);
-
-        done();
     });
 
-    test('it should throw if a schema with provided name already exists', (done) => {
+    it('should throw if a schema with provided name already exists', () => {
         const joiManager = new JoiManager();
 
         expect(() => joiManager.add(schemaName, schema)).not.toThrow();
         expect(() => joiManager.add(schemaName, schema)).toThrow(Error);
-
-        done();
     });
 
-    test('it should be chainable', (done) => {
+    it('should be chainable', () => {
         const joiManager = new JoiManager();
 
         expect(() => joiManager.add(schemaName, schema).add(schemaName2, schema)).not.toThrow();
 
         expect(joiManager.schemaList).toHaveProperty(schemaName, schema);
         expect(joiManager.schemaList).toHaveProperty(schemaName2, schema);
-
-        done();
     });
 });
 
-describe('It should have "get" method', () => {
+describe('JoiManager#get()', () => {
     const schemaName = 'random-schema-name/number';
     const schemaName2 = 'random-schema-name/number2';
     const schema = Joi.number().min(0).max(1);
 
-    test('it should get schemas from the list', (done) => {
+    it('should get schemas from the list', () => {
         const joiManager = new JoiManager();
 
         joiManager.add(schemaName, schema);
@@ -112,11 +96,9 @@ describe('It should have "get" method', () => {
 
         expect(joiManager.get(schemaName)).toBe(schema);
         expect(joiManager.get(schemaName2)).toBe(schema);
-
-        done();
     });
 
-    test('it should throw if "schemaName" is not a string', (done) => {
+    it('should throw if "schemaName" is not a string', () => {
         const joiManager = new JoiManager();
 
         expect(() => joiManager.get()).toThrow(TypeError);
@@ -124,20 +106,16 @@ describe('It should have "get" method', () => {
         expect(() => joiManager.get(OBJECT__)).toThrow(TypeError);
         expect(() => joiManager.get(NULL__)).toThrow(TypeError);
         expect(() => joiManager.get(SYMBOL__)).toThrow(TypeError);
-
-        done();
     });
 
-    test('it should throw if a schema with provided name was not added to the list', (done) => {
+    it('should throw if a schema with provided name was not added to the list', () => {
         const joiManager = new JoiManager();
 
         expect(() => joiManager.get(schemaName)).toThrow(Error);
         expect(() => joiManager.get(schemaName2)).toThrow(Error);
-
-        done();
     });
 
-    test('it should support nested schemas', (done) => {
+    it('should support nested schemas', () => {
         const joiManager = new JoiManager();
         const nestedSchemaName = 'nested-schema';
 
@@ -154,17 +132,15 @@ describe('It should have "get" method', () => {
 
         expect(joiManager.schemaList).toHaveProperty(schemaName, schema);
         expect(joiManager.schemaList).toHaveProperty(nestedSchemaName);
-
-        done();
     });
 });
 
-describe('It should have "validate" method', () => {
+describe('JoiManager#validate()', () => {
     const schemaName = 'random-schema-name/number';
     const schema = Joi.number().min(0).max(1);
     const correctValue = 0.5;
 
-    test('it should return a promise', (done) => {
+    it('should return a promise', (done) => {
         const joiManager = new JoiManager();
         joiManager.add(schemaName, schema);
 
@@ -175,7 +151,7 @@ describe('It should have "validate" method', () => {
         });
     });
 
-    test('it should reject a promise if validation failed', (done) => {
+    it('should reject a promise if validation failed', (done) => {
         const joiManager = new JoiManager();
         joiManager.add(schemaName, schema);
 
@@ -186,15 +162,13 @@ describe('It should have "validate" method', () => {
         });
     });
 
-    test('it should throw if a schema with provided name was not added to the list', (done) => {
+    it('should throw if a schema with provided name was not added to the list', () => {
         const joiManager = new JoiManager();
 
         expect(() => joiManager.validate(NUMBER__, schemaName)).toThrow(Error);
-
-        done();
     });
 
-    test('it should throw if "defaultOptions" is not an object', (done) => {
+    it('should throw if "defaultOptions" is not an object', () => {
         const joiManager = new JoiManager();
         joiManager.add(schemaName, schema);
 
@@ -202,7 +176,5 @@ describe('It should have "validate" method', () => {
         expect(() => joiManager.validate(NUMBER__, schemaName, NUMBER__)).toThrow(TypeError);
         expect(() => joiManager.validate(NUMBER__, schemaName, STRING__)).toThrow(TypeError);
         expect(() => joiManager.validate(NUMBER__, schemaName, SYMBOL__)).toThrow(TypeError);
-
-        done();
     });
 });
